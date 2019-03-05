d5af3939630453a54e5dfe11014ec9f15ae664f6
Angelo Dini <angelo.dini@divio.ch>
2019-03-05 14:25:16 +0100

    another test

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
  <body>
  <?php echo $_SERVER['DEFAULT_DATABASE_DSN']; ?>
  <?php echo $_SERVER['DATABASE_URL']; ?>
+ <?php echo getenv('DATABASE_URL'); ?>
+ <?php echo getenv('DEFAULT_DATABASE_DSN'); ?>
   <?php echo $argv; ?>
   <?php phpinfo(); ?>
  </body>
