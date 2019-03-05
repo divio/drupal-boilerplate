633ea2bd7c389c61cd54a5a7f3e761693deb6b89
Angelo Dini <angelo.dini@divio.ch>
2019-03-05 14:22:24 +0100

    test

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
   <title>PHP Project</title>
  </head>
  <body>
-  <?php echo $_SERVER['DEFAULT_DATABASE_DSN']; ?>
+ <?php echo $_SERVER['DEFAULT_DATABASE_DSN']; ?>
+ <?php echo $_SERVER['DATABASE_URL']; ?>
   <?php echo $argv; ?>
   <?php phpinfo(); ?>
  </body>
