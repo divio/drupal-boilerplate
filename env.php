c2f715840accd2f9674d719c3882bc0262109c10
Angelo Dini <angelo.dini@divio.ch>
2019-03-06 11:34:28 +0100

    blah

--- env.php
+++ env.php
@@ -1,5 +1,5 @@
-<?php
-    echo getenv('DEFAULT_DATABASE_DSN');
-    echo "\n";
-    echo getenv('DEFAULT_STORAGE_DSN');
-?>
+<h6>DEFAULT_DATABASE_DSN</h6>
+<p><code><?php echo getenv('DEFAULT_DATABASE_DSN'); ?></code></p>
+
+<h6>DEFAULT_STORAGE_DSN</h6>
+<p><code><?php echo getenv('DEFAULT_STORAGE_DSN'); ?></code></p>
